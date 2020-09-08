710891db44b32c68653f76c74432df9314b5d11e
cesarvin <vinizio007@gmail.com>
2020-09-07 18:45:30 -0600

    DR1

---
+++ gl.py
@@ -0,0 +1,232 @@
+import struct
+
+from obj import Obj
+from collections import namedtuple
+from gl_aux import *
+
+#import numpy as np
+#from numpy import matrix, cos, sin
+
+V2 = namedtuple('Point2', ['x', 'y'])
+V3 = namedtuple('Point3', ['x', 'y', 'z'])
+V4 = namedtuple('Point4', ['x', 'y', 'z','w'])
+
+def char(c):
+    # 1 byte
+    return struct.pack('=c', c.encode('ascii'))
+
+def word(w):
+    # 2 bytes
+    return struct.pack('=h',w)
+
+def dword(d):
+    # 4 bytes
+    return struct.pack('=l',d)
+
+def color(r, g, b):
+    return bytes([int(b * 255), int(g * 255), int(r * 255)])
+
+
+def baryCoords(A, B, C, P):
+    # u es para la A, v es para B, w para C
+    try:
+        u = ( ((B.y - C.y)*(P.x - C.x) + (C.x - B.x)*(P.y - C.y) ) /
+              ((B.y - C.y)*(A.x - C.x) + (C.x - B.x)*(A.y - C.y)) )
+
+        v = ( ((C.y - A.y)*(P.x - C.x) + (A.x - C.x)*(P.y - C.y) ) /
+              ((B.y - C.y)*(A.x - C.x) + (C.x - B.x)*(A.y - C.y)) )
+
+        w = 1 - u - v
+    except:
+        return -1, -1, -1
+
+    return u, v, w
+
+BLACK = color(0,0,0)
+WHITE = color(1,1,1)
+
+class Raytracer(object):
+    def __init__(self, width, height):
+        self.backcolor = BLACK
+        self.pointcolor = WHITE
+        #self.light = V3(0,0,1)
+        #self.active_texture = None
+        #self.active_shader = None
+        self.glCreateWindow(width, height)
+        self.camPosition = V3(0,0,0)
+        self.fov = 60
+        self.scene = []
+
+    #def glInit(self):
+    #    self.pixels =[]
+    
+    def glCreateWindow(self, width, height):
+        self.width = width
+        self.height = height
+        self.glClear()
+        self.glViewPort(0, 0, width, height)
+        #self.createViewMatrix()
+        #self.createProjectionMatrix()
+
+    def glViewPort(self, x, y, width, height):
+        self.glViewPortWidth = width - 1
+        self.glViewPortHeight = height - 1
+        self.glViewPortX = x
+        self.glViewPortY = y
+        self.viewportMatrix = matrix([[width/2,        0,   0,  x + width/2],
+                                      [      0, height/2,   0, y + height/2],
+                                      [      0,        0, 0.5,          0.5],
+                                      [      0,        0,   0,            1]])
+
+    def glClear(self):
+        self.pixels = [ [ self.backcolor for x in range(self.width)] for y in range(self.height) ]
+        self.zbuffer = [ [ float('inf') for x in range(self.width)] for y in range(self.height) ]
+
+    def glClearColor(self, r, g, b):
+        self.backcolor = color(r, g, b) 
+        self.pixels = [ [ self.backcolor for x in range(self.width)] for y in range(self.height) ]
+        self.zbuffer = [ [ float('inf') for x in range(self.width)] for y in range(self.height) ]
+
+    def glVertex(self, x, y):
+        glVertexX = ( x + 1 ) * ( self.glViewPortWidth / 2 ) + self.glViewPortX 
+        glVertexY = ( y + 1 ) * ( self.glViewPortHeight / 2) + self.glViewPortY 
+        #print (round(glVertexX))
+        #print (round(glVertexY))
+        self.pixels[round(glVertexY)][round(glVertexX)] = self.pointcolor
+
+    def glColor(self, r, g, b):
+        self.pointcolor = color(r, g, b)
+
+    def glFinish(self, filename='out.bmp'):
+        self.write(filename)        
+    
+    def write(self, filename):
+        # Función write basada en ejemplo realizado en clase
+        archivo = open(filename, 'wb')
+
+        # File header 14 bytes
+        archivo.write(char('B'))
+        archivo.write(char('M'))
+
+        archivo.write(dword(14 + 40 + self.width * self.height * 3))
+        archivo.write(dword(0))
+        archivo.write(dword(14 + 40))
+
+        # Image Header 40 bytes
+        archivo.write(dword(40))
+        archivo.write(dword(self.width))
+        archivo.write(dword(self.height))
+        archivo.write(word(1))
+        archivo.write(word(24))
+        archivo.write(dword(0))
+        archivo.write(dword(self.width * self.height * 3))
+        archivo.write(dword(0))
+        archivo.write(dword(0))
+        archivo.write(dword(0))
+        archivo.write(dword(0))
+
+        # Pixeles, 3 bytes cada uno
+
+        for x in range(self.height):
+            for y in range(self.width):
+                archivo.write(self.pixels[x][y])
+
+        archivo.close()
+
+    def point(self, x, y, color = None):
+        if x < self.glViewPortX or x >= self.glViewPortX + self.glViewPortWidth or y < self.glViewPortY or y >= self.glViewPortY + self.glViewPortHeight:
+            return
+
+        if x >= self.width or x < 0 or y >= self.height or y < 0:
+            return
+
+        try:
+            self.pixels[y][x] = color or self.pointcolor
+        except:
+            pass
+
+    
+    def glZBuffer(self, filename):
+        archivo = open(filename, 'wb')
+
+        # File header 14 bytes
+        archivo.write(bytes('B'.encode('ascii')))
+        archivo.write(bytes('M'.encode('ascii')))
+        archivo.write(dword(14 + 40 + self.width * self.height * 3))
+        archivo.write(dword(0))
+        archivo.write(dword(14 + 40))
+
+        # Image Header 40 bytes
+        archivo.write(dword(40))
+        archivo.write(dword(self.width))
+        archivo.write(dword(self.height))
+        archivo.write(word(1))
+        archivo.write(word(24))
+        archivo.write(dword(0))
+        archivo.write(dword(self.width * self.height * 3))
+        archivo.write(dword(0))
+        archivo.write(dword(0))
+        archivo.write(dword(0))
+        archivo.write(dword(0))
+
+        minZ = float('inf')
+        maxZ = -float('inf')
+        for x in range(self.height):
+            for y in range(self.width):
+                if self.zbuffer[x][y] != -float('inf'):
+                    if self.zbuffer[x][y] < minZ:
+                        minZ = self.zbuffer[x][y]
+
+                    if self.zbuffer[x][y] > maxZ:
+                        maxZ = self.zbuffer[x][y]
+
+        for x in range(self.height):
+            for y in range(self.width):
+                depth = self.zbuffer[x][y]
+                if depth == -float('inf'):
+                    depth = minZ
+                
+                if (maxZ - minZ) > 0:
+                    depth = (depth - minZ) / (maxZ - minZ)
+                else:
+                    depth = (depth - minZ)
+
+                archivo.write(color(depth,depth,depth))
+                
+
+        archivo.close()
+
+    
+    def rtRender(self):
+        #recorre pixel por pixel
+        for y in range(self.height):
+            for x in range(self.width):
+
+                Px = 2 * ( ( x + 0.5 ) / self.width ) - 1
+                Py = 2 * ( ( y + 0.5 ) / self.height ) - 1
+
+                #FOV(angulo de vision), asumiendo que el near plane esta a 1 unidad de la camara
+                t = tan( (self.fov * np.pi / 180) / 2 )
+                r = t * self.width / self.height
+                Px *= r
+                Py *= t
+
+                #Nuestra camara siempre esta viendo hacia -Z
+                direction = V3(Px, Py, -1)
+                normal_direction = vectNormal(direction)
+                direction = V3(direction.x / normal_direction, direction.y / normal_direction, direction.z / normal_direction)
+
+                material = None
+
+                for obj in self.scene:
+                    intersect = obj.ray_intersect(self.camPosition, direction)
+                    if intersect is not None:
+                        if intersect.distance < self.zbuffer[y][x]:
+                            self.zbuffer[y][x] = intersect.distance
+                            material = obj.material
+
+                if material is not None:
+                    self.point(x, y, material.diffuse)
+
+
+                        
---
+++ snowman.py
@@ -0,0 +1,40 @@
+from gl import Raytracer, color, V2, V3
+from obj import Obj, Texture
+from sphere import Sphere, Material
+import random
+
+brick = Material(diffuse = color(0.8, 0.25, 0.25 ))
+stone = Material(diffuse = color(0.4, 0.4, 0.4 ))
+grass = Material(diffuse = color(0.5, 1, 0))
+
+snowman = Material(diffuse = color(1, 1, 1))
+button = Material(diffuse = color(0, 0, 0))
+nose = Material(diffuse = color(1, 0.5, 0))
+
+width = 960
+height = 1280
+r = Raytracer(width,height)
+#r.glCreateWindow(500,300)
+
+r.glClearColor(0.3,0.5,0.8)
+
+r.scene.append( Sphere(V3(0.15, 1.5,  -4), 0.05, button) )
+r.scene.append( Sphere(V3(-0.15, 1.5,  -4), 0.05, button) )
+r.scene.append( Sphere(V3(0, 1.3,  -4), 0.10, nose) )
+
+r.scene.append( Sphere(V3(0.20, 1.10,  -4), 0.04, button) )
+r.scene.append( Sphere(V3(-0.10, 1.15,  -4), 0.04, button) )
+r.scene.append( Sphere(V3(0.10, 1.15,  -4), 0.04, button) )
+r.scene.append( Sphere(V3(-0.20, 1.10,  -4), 0.04, button) )
+
+#r.scene.append( Sphere(V3(0, 1.1,  -6), 0.18, button) )
+#r.scene.append( Sphere(V3(0, 0.2,  -6), 0.20, button) )
+#r.scene.append( Sphere(V3(0, -0.8,  -6), 0.22, button) )
+
+#r.scene.append( Sphere(V3(0, 2.6,  -8), 0.90, snowman) )
+#r.scene.append( Sphere(V3(0, 1,  -8), 1.25, snowman) )
+#r.scene.append( Sphere(V3(0, -1.1,  -8), 1.6, snowman) )
+ 
+r.rtRender()
+
+r.glFinish('output8.bmp')
